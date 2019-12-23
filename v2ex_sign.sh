53120ad7c0ce519cb1591b502d1c8c37b369e487
dzlzh <dzlzh@null.net>
2019-12-23 16:48:17 +0800

    feat(V2ex): 添加签到脚本

---
+++ v2ex_sign.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eux
+
+LANGUAGE="zh-CN"
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -H "Cookie: $V2EX_COOKIE" \
+  https://www.v2ex.com/mission/daily/
