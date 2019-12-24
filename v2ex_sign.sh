efb40db275b0e5cd277762f83680a6a12e0e81ab
dzlzh <dzlzh@null.net>
2019-12-24 14:20:05 +0800

    feat(V2ex): 邮箱推送更换为 Server 酱

--- v2ex_sign.sh
+++ v2ex_sign.sh
@@ -2,28 +2,19 @@
 
 set -eux
 
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-BASE_URL='https://www.v2ex.com/mission/daily/'
+base_url='https://www.v2ex.com/mission/daily/'
 
 function sendRequest() {
     curl \
-      -H "User-Agent: $UA" \
+      -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36" \
       -H "Cookie: $V2EX_COOKIE" \
       -o result.html \
       $1
 }
 
-sendRequest $BASE_URL
-
-SIGN_URL=$BASE_URL`sed -n "s/.*\/mission\/daily\/\(redeem?once=\w*\).*/\1/p" result.html`
-sendRequest $SIGN_URL
-
-sendRequest $BASE_URL
+sendrequest $base_url
+sign_url=$base_url`sed -n "s/.*\/mission\/daily\/\(redeem?once=\w*\).*/\1/p" result.html`
+sendrequest $sign_url
+sendrequest $base_url
 message=$(sed -n "s/.*\(每日登录奖励已领取\).*/\1/p" result.html)
-curl \
-    -s --user "api:$MAIL_API_KEY" \
-    https://api.mailgun.net/v3/$MAIL_DOMAIN/messages \
-    -F from="Notification <mailgun@$MAIL_DOMAIN>" \
-    -F to=$MAIL_TO \
-    -F subject="$(date "+v2ex %Y-%m-%d")" \
-    -F text="${message:-失败}"
+curl https://sc.ftqq.com/$SCKEY.send?text=$(date "+%Y-%m-%d")-V2ex-${message:-失败}
