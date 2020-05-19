00f0a3c9a329a8efb1ad354e372c1963fe280efd
dzlzh <dzlzh@null.net>
2020-05-19 21:46:20 +0800

    hacpai

--- hacpai.sh
+++ hacpai.sh
@@ -6,18 +6,18 @@ function sendRequest() {
     curl \
       -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36" \
       -b "$cookie" \
+      -c "cookie.txt" \
       -i \
       -o result.html \
       $1
 }
 
-base_url='https://hacpai.com/activity/checkin'
 cookie=$HACPAI_COOKIE
+base_url='https://hacpai.com/activity/checkin'
 sendRequest $base_url
+cookie='cookie.txt'
 sign_url=$(sed -n "s/.*\(https:\/\/hacpai.com\/activity\/daily-checkin?token=\w*\).*/\1/p" result.html)
-cookie=$(sed -n "s/set-cookie: \([^;]*\);.*/\1/p" result.html)
 sendRequest $sign_url
-cookie=$(sed -n "s/set-cookie: \([^;]*\);.*/\1/p" result.html)
 sendRequest $base_url
 message=$(sed -n "s/.*今日签到获得[^>]*>\([0-9]*\).*/\1/p" result.html)
 
