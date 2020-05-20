1c7f7f0d95da7729d562668389b50435a60f6ada
dzlzh <dzlzh@null.net>
2020-05-20 10:15:33 +0800

    hacpai

--- hacpai.sh
+++ hacpai.sh
@@ -4,6 +4,7 @@ set -eux
 
 function sendRequest() {
     curl \
+      -L \
       -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36" \
       -b "$cookie" \
       -c "cookie.txt" \
@@ -13,12 +14,9 @@ function sendRequest() {
 }
 
 cookie=$HACPAI_COOKIE
-base_url='https://hacpai.com/activity/checkin'
-sendRequest $base_url
+sendRequest 'https://hacpai.com/activity/checkin'
 cookie='cookie.txt'
-sign_url=$(sed -n "s/.*\(https:\/\/hacpai.com\/activity\/daily-checkin?token=\w*\).*/\1/p" result.html)
-sendRequest $sign_url
-sendRequest $base_url
+sendRequest $(sed -n "s/.*\(https:\/\/hacpai.com\/activity\/daily-checkin?token=\w*\).*/\1/p" result.html)
 message=$(sed -n "s/.*今日签到获得[^>]*>\([0-9]*\).*/\1/p" result.html)
 
 curl https://sc.ftqq.com/$SCKEY.send?text=$(date "+%Y-%m-%d")-Hacpai-${message:-失败}
